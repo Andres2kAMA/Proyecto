e121687d7c015ad9421160bcfc8883d07e60bdd9
Fernando <fernandomartinez.alu@iespacomolla.es>
2022-02-07 19:01:00 +0100

    Actualizo la ruta de datosFirebase

--- js/datosFirebase/datosFirebase.js
+++ js/datosFirebase/datosFirebase.js
@@ -1,6 +1,6 @@
 "use strict";
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js";
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.1/firebase-auth.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBfP9idYMJIJ0u4Z5jZ0UNidCv3JELR0oQ",
